0a33d7ae285812c6311ef1a0ef5c105b9ecee066
Rasl <aitekov_rasul@mail.ru>
2019-08-19 11:50:28 +0300

    Initial commit

---
+++ Notes/Sources/Screens/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by rasl on 19/08/2019.
+//  Copyright © 2019 com.rasl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		title = "Notes"
+		view.backgroundColor = .white
+		
+	}
+}
+
